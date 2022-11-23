4e671167a1d6ed179b3f067867a5e4394943f6ea
PABMAXICHAC <116607572+PABMAXICHAC@users.noreply.github.com>
2022-11-23 18:10:22 -0300

    Add files via upload
    please dont steal !

---
+++ smiler_2_spawn_plus_lights.lua
@@ -0,0 +1,3 @@
+firesignal(game.ReplicatedStorage.Bricks.UseEventModule.OnClientEvent, "flickerLights", game.ReplicatedStorage.GameData.LatestRoom.Value, 10)
+wait(12)
+loadstring(game:HttpGet("https://github.com/PABMAXICHAC/doors-monsters-scripts/raw/main/Smiler_2_TESTING.lua"))()
