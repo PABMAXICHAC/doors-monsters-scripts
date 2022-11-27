8a046933d55c5a7751d82bee5be2b9d92f574a20
PABMAXICHAC <116607572+PABMAXICHAC@users.noreply.github.com>
2022-11-27 03:02:43 -0300

    Add files via upload
    wow!!!1

---
+++ achievement_survive_wh1t3.lua
@@ -0,0 +1,9 @@
+local Achievements = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+-- Creates and displays your custom achievement
+Achievements.Get({
+    Title = "Fought a beast and survived.",
+    Desc = "HOW",
+    Reason = "Survive WH1T3.",
+    Image = "https://github.com/PABMAXICHAC/doors-monsters-models/blob/main/MOSHED-2022-11-27-1-33-26.jpg?raw=true",
+})
